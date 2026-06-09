ad388e6962287cd7a96d88a45babc353a676c302
Taoufiq Benallah <taoufiq.benallah@gmail.com>
2026-06-09 05:54:50 +0100

    Adding Signup with Google + Login with Google

--- apps/website/src/app/layout.tsx
+++ apps/website/src/app/layout.tsx
@@ -1,30 +1,18 @@
-"use client";
-
-import type { Metadata } from "next";
 import "./globals.css";
-import { usePathname } from "next/navigation";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import FloatingContact from "@/components/FloatingContact";
+import Providers from "../components/Providers";
+import LayoutContent from "../components/LayoutContent";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const pathname = usePathname();
-
-  const isChatPage = pathname.startsWith("/chat");
-
   return (
     <html lang="en">
       <body>
-        {!isChatPage && <Header />}
-
-        <main>{children}</main>
-
-        {!isChatPage && <Footer />}
-        {!isChatPage && <FloatingContact />}
+        <Providers>
+          <LayoutContent>{children}</LayoutContent>
+        </Providers>
       </body>
     </html>
   );
