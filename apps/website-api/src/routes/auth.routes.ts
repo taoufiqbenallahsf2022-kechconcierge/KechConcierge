bac74fea0e1574d1e2640ccbc4e8f0ec90817c8e
Taoufiq Benallah <taoufiq.benallah@gmail.com>
2026-06-09 13:48:13 +0100

    create login endpoints

--- apps/website-api/src/routes/auth.routes.ts
+++ apps/website-api/src/routes/auth.routes.ts
@@ -2,15 +2,17 @@ import { Router } from "express";
 import {
   checkEmail,
   signup,
+  login,
   verifyEmail,
-  googleSignupController,
+  googleAuthController,
 } from "../controllers/auth.controller";
 
 const router = Router();
 
 router.post("/check-email", checkEmail);
 router.post("/signup", signup);
+router.post("/login", login);
 router.get("/verify-email", verifyEmail);
-router.post("/google-auth", googleSignupController);
+router.post("/google-auth", googleAuthController);
 
 export default router;
